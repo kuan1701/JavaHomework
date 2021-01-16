23609215ceb321696f7abaa522d189e006c5a697
kuan <62588557+kuan1701@users.noreply.github.com>
2021-01-16 17:06:54 +0300

    task 4

--- src/HomeWork_2/GuessTheNumber.java
+++ src/HomeWork_2/GuessTheNumber.java
@@ -1,3 +1,15 @@
+/**
+ *
+ * GuessTheNumber implements an application that simply displays the
+ * computer-guessed number entered by the user. The program asks the
+ * user for an integer value from 0 to 100. Then the computer uses a
+ * loop to select a number equal to the number entered by the user.
+ * Displays a message in which the number entered by the user is written,
+ * and the number of attempts for which the computer found this number.
+ *
+ * @version 15.0.1
+ */
+
 package HomeWork_2;
 
 import java.util.Random;
@@ -13,18 +25,26 @@ public class GuessTheNumber {
 		
 		System.out.println("Please a enter your number between 0 and 100!!");
 		
-		int userNum = num.nextInt();;
+		int userNum = num.nextInt();
 		int compNum;
 		int count = 0;
 		
-		do {
+		//Checking the number entered by user
+		if (userNum > 100) {
+			System.out.println("You entered a number greater than 100!!!");
+		} else if (userNum < 0) {
+			System.out.println("You entered a number less than 0!!!");
+		} else {
 			
-			count++;
-			compNum = RND.nextInt(100);
+			//Loop for guessing the number entered by user
+			do {
+				count++;
+				compNum = RND.nextInt(100);
+			} while (compNum != userNum);
 			
-		} while (compNum != userNum);
-		
-		System.out.println("Your number is: " + compNum);
-		System.out.println("I got it right on " + count + " try.");
+			System.out.println("Your number is: " + compNum);
+			System.out.println("I got it right on " + count + " try.");
+		}
 	}
 }
+
